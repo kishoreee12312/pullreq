271bddd61a2e4bfbc7d4bd89486f829182795774
kishoreee12312 <kishorevkishore005@gmail.com>
2024-12-11 15:49:57 +0530

    Update main.java

--- main.java
+++ main.java
@@ -11,9 +11,9 @@ c=x+y;
 public static void main(String[] args)
 {
 Scanner obj = new Scanner(System.in);
-System.out.println("enter the number :");
+System.out.println("enter num:");
 int a = obj.nextInt();
-System.out.println("enter the number :");
+System.out.println("enter num:");
 int b = obj.nextInt();
 main myobj = new main(a,b);
 System.out.println(myobj.c);
